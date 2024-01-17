ee634845ce2ef8c2ade0d445dbcce17d9b00d887
najwanap12 <najwanap.1201@gmail.com>
2024-01-17 11:39:51 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,157 @@
+import { Image, Text, StyleSheet, View, TouchableOpacity, Alert, ScrollView } from 'react-native';
+
+
+
+export default function App() {
+  return (
+    <ScrollView>
+   <View style={styles.container}>
+   
+   <Image 
+        style={styles.avatar}
+        source={require('./assets/me.jpeg')}
+      />
+    <Text style={styles.title}>Naya</Text>
+    <Text style={styles.subtitle}>Halo! Nama saya Najwa Nararya Ayuni Putri, biasa dipanggil Naya. Saya lahir di Purwokerto, 12 Januari 2006. Saya bersekolah di SMK Telkom Purwokerto dengan jurusan RPL. </Text>
+     <TouchableOpacity style={styles.button} onPress={ () => Alert.alert('+6213456789')}>
+        <Text style={styles.text}>Contact Me</Text>
+      </TouchableOpacity>
+      <Text style={styles.title2}>My Recent Project</Text>
+      <Image 
+        style={styles.avatar2}
+        source={require('./assets/wcj.png')}
+      />
+      <Text style={styles.subtitle2}> WISH CLEAN JUICE </Text>
+      <Text style={styles.subtitle5}> Membuat design web e-commerce yang menjual jus sehat. </Text>
+      <Image 
+        style={styles.avatar3}
+        source={require('./assets/dietyuk.png')}
+      />
+      <Text style={styles.subtitle3}> DIET YUK </Text>
+      <Text style={styles.subtitle5}> Membuat design aplikasi yang membantu progress diet. </Text>
+      <Image 
+        style={styles.avatar4}
+        source={require('./assets/arisan.png')}
+      />
+      <Text style={styles.subtitle4}> ARISAN SAY </Text>
+      <Text style={styles.subtitle5}> Membuat design web untuk arisan online. </Text>
+      <Image 
+        style={styles.avatar5}
+        source={require('./assets/redesign.png')}
+      />
+      <Text style={styles.subtitle6}> SUPER CAT </Text>
+      <Text style={styles.subtitle5}> Meredesign produk makanan basah kucing. </Text>
+   </View>
+   </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: 'black',
+    alignItems: 'center',
+  },
+
+  title: {
+    color: '#f47373',
+    fontSize: 25,
+    fontWeight: 'bold',
+    padding: 5,
+    textAlign: 'center',
+  },
+  subtitle: {
+    color: 'white',
+    fontSize: 12,
+    paddingBottom: 15,
+    textAlign: 'center',
+  },
+  avatar: {
+    width: 150,
+    height: 150,
+    fontweight: 'bold',
+    borderRadius: 100,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#f47373',
+    padding: 10,
+    borderRadius: 10,
+  },
+  text: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
+   title2: {
+    color: '#f47373',
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 15,
+    textAlign: 'center',
+  },
+  avatar2: {
+    width: 300,
+    height: 180,
+    fontweight: 'bold',
+    borderRadius: 5,
+    textAlign: 'center',
+  },
+   subtitle2: {
+    color: '#f47373',
+    fontSize: 15,
+    fontWeight: 'bold',
+    paddingTop: 5,
+    textAlign: 'center',
+  },
+  avatar3: {
+    width: 300,
+    height: 200,
+    fontweight: 'bold',
+    borderRadius: 5,
+    
+  },
+   subtitle3: {
+    color: '#f47373',
+    fontSize: 15,
+    fontWeight: 'bold',
+    paddingTop: 5,
+    textAlign: 'center',
+  },
+  avatar4: {
+    width: 300,
+    height: 180,
+    fontweight: 'bold',
+    borderRadius: 5,
+    
+  },
+   subtitle4: {
+    color: '#f47373',
+    fontSize: 15,
+    fontWeight: 'bold',
+    paddingTop: 5,
+    textAlign: 'center',
+  },
+  subtitle5: {
+    color: 'white',
+    fontSize: 12,
+    paddingBottom: 15,
+    textAlign: 'center',
+  },
+  avatar5: {
+    width: 300,
+    height: 180,
+    fontweight: 'bold',
+    borderRadius: 5,
+    
+  },
+   subtitle6: {
+    color: '#f47373',
+    fontSize: 15,
+    fontWeight: 'bold',
+    paddingTop: 5,
+    textAlign: 'center',
+  },
+});
+
+
